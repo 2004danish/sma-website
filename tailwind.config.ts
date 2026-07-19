14392cb4d28b0bb9d8342b99ff7d2b2945b33a8c
2004danish <rajeshsondawale14060@gmail.com>
2026-07-20 00:20:34 +0530

    chore: add/update tailwind.config.ts for App Router

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#0A0A0A",
+        foreground: "#FAFAFA",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
